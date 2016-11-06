7af9dc1c561ada5166ba19b2d52c0f9ed0937012
Victor <victor.gx15@gmail.com>
2016-11-06 21:26:02 +0100

    Add some comments

--- Source.cpp
+++ Source.cpp
@@ -8,11 +8,15 @@ using namespace std;
 
 int main() {
 	vector<int> X; int N = 0;
+	
+	// How many random variables the user wants ?
 	while (N < 10 || N > 2500) cin >> N;
+	
+	// The user chooses the value of each ramdom variable
 	for (int i = 0; i < N; i++)
 	{
 		int xi = -1;
-		while (xi < 0 || xi > 10e5)	cin >> xi;
+		while (xi < 0 || xi > 10e5) cin >> xi;
 		X.push_back(xi);
 	}
 
@@ -21,7 +25,11 @@ int main() {
 	double median = (N % 2 == 0) ? (X[N / 2 - 1] + X[N / 2]) / 2.0 : X[N / 2];
 
 	map<int, int> f; int mode = 0;
+	
+	// Counts the number of appearance of each diffent random variable
 	for (int e : X)	f[e]++;
+	
+	// Seach for the one which has the most appearance
 	for (auto e : f) if (e.second > f[mode]) mode = e.first;
 
 	cout << mean << endl;
